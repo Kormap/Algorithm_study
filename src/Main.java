8e9a3bc430adb1a39428cf8c80dc1b688d1a12be
kodh1000 <kodh10@gmail.com>
2022-12-04 23:42:32 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+//메인 클래스
+public class Main {
+
+    public static void main(String[] args) {
+        FastReader fr=new FastReader();
+
+                int A = fr.nextInt();
+                int B = fr.nextInt();
+                int C = fr.nextInt();
+
+                int[] arr = new int[3];
+
+                arr[0] = A;
+                arr[1] = B;
+                arr[2] = C;
+                Arrays.sort(arr);
+                System.out.println(arr[0]);
+        }
+
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
